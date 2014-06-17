fcd2c5a6a4efb6d807bf898a1c0f40dabba0337b
Sebastien Badia <sebastien.badia@enovance.com>
2014-06-17 18:09:01 +0200

    [params] Update trove::params class
    Update params class with corresponding packages names

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,9 +5,17 @@ class trove::params {
   case $::osfamily {
     'RedHat': {
       $client_package_name      = 'openstack-trove'
+      $common_package_name      = 'openstack-trove-common'
+      $api_package_name         = 'openstack-trove-api'
+      $guestagent_package_name  = 'openstack-trove-guestagent'
+      $taskmanager_package_name = 'openstack-trove-taskmanager'
     }
     'Debian': {
       $client_package_name      = 'python-troveclient'
+      $common_package_name      = 'trove-common'
+      $api_package_name         = 'trove-api'
+      $guestagent_package_name  = 'trove-guestagent'
+      $taskmanager_package_name = 'trove-taskmanager'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
