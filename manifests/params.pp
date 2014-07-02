1caefc79fc94f3fae87f90c3ca8ca6931c00f4e8
Sebastien Badia <sebastien.badia@enovance.com>
2014-07-02 14:01:37 +0200

    conductor: Introduce conductor support

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,16 +6,26 @@ class trove::params {
     'RedHat': {
       $client_package_name      = 'openstack-trove'
       $common_package_name      = 'openstack-trove-common'
+      $conductor_package_name   = 'openstack-trove-conductor'
+      $conductor_service_name   = 'openstack-trove-conductor'
       $api_package_name         = 'openstack-trove-api'
+      $api_service_name         = 'openstack-trove-api'
       $guestagent_package_name  = 'openstack-trove-guestagent'
+      $guestagent_service_name  = 'openstack-trove-guestagent'
       $taskmanager_package_name = 'openstack-trove-taskmanager'
+      $taskmanager_service_name = 'openstack-trove-taskmanager'
     }
     'Debian': {
       $client_package_name      = 'python-troveclient'
       $common_package_name      = 'trove-common'
+      $conductor_package_name   = 'trove-conductor'
+      $conductor_service_name   = 'trove-conductor'
       $api_package_name         = 'trove-api'
+      $api_service_name         = 'trove-api'
       $guestagent_package_name  = 'trove-guestagent'
+      $guestagent_service_name  = 'trove-guestagent'
       $taskmanager_package_name = 'trove-taskmanager'
+      $taskmanager_service_name = 'trove-taskmanager'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
