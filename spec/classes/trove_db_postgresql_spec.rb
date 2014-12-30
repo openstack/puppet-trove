edde7da295d2a2c3544ea28694bb3e846f89a96d
Sebastien Badia <sebastien.badia@enovance.com>
2014-12-30 22:26:48 +0100

    Migrate postgresql backend to use openstacklib::db::postgresql
    Let migrate to the new openstacklib::db::postgresql helper for
    postgresql backend. This commit also unpin postgresql fixture
    (openstacklib support now the latest version of postgre module).
    
    Change-Id: I9bf307d758c112a9b458dab3dfba26281278e38f
    Implements: blueprint commmon-openstack-database-resource

--- spec/classes/trove_db_postgresql_spec.rb
+++ spec/classes/trove_db_postgresql_spec.rb
@@ -3,24 +3,56 @@ require 'spec_helper'
 describe 'trove::db::postgresql' do
 
   let :req_params do
-    {:password => 'pw'}
+    { :password => 'pw' }
   end
 
-  let :facts do
-    {
-      :postgres_default_version => '8.4',
-      :osfamily => 'RedHat',
-    }
+  let :pre_condition do
+    'include postgresql::server'
   end
 
-  describe 'with only required params' do
-    let :params do
-      req_params
+  context 'on a RedHat osfamily' do
+    let :facts do
+      {
+        :osfamily                 => 'RedHat',
+        :operatingsystemrelease   => '7.0',
+        :concat_basedir => '/var/lib/puppet/concat'
+      }
     end
-    it { is_expected.to contain_postgresql__db('trove').with(
-      :user         => 'trove',
-      :password     => 'pw'
-     ) }
+
+    context 'with only required parameters' do
+      let :params do
+        req_params
+      end
+
+      it { is_expected.to contain_postgresql__server__db('trove').with(
+        :user     => 'trove',
+        :password => 'md5e12ef276d200761a0808f17a5b076451'
+      )}
+    end
+
+  end
+
+  context 'on a Debian osfamily' do
+    let :facts do
+      {
+        :operatingsystemrelease => '7.8',
+        :operatingsystem        => 'Debian',
+        :osfamily               => 'Debian',
+        :concat_basedir => '/var/lib/puppet/concat'
+      }
+    end
+
+    context 'with only required parameters' do
+      let :params do
+        req_params
+      end
+
+      it { is_expected.to contain_postgresql__server__db('trove').with(
+        :user     => 'trove',
+        :password => 'md5e12ef276d200761a0808f17a5b076451'
+      )}
+    end
+
   end
 
 end
