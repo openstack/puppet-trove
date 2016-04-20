b0804e5f8a5869805ef9ec46d4828a0b2f247687
Alexey Deryugin <aderyugin@mirantis.com>
2016-04-20 11:57:37 +0300

    Use oslo module for logging and database configuration
    Additonal changes:
    
      * switch database related parameters to $::os_service_default
        as they are used in oslo::db
      * update unit tests
    
    Change-Id: I2c79261d8000d68521f783b5df9a535ff47ce53d

--- spec/classes/trove_db_spec.rb
+++ spec/classes/trove_db_spec.rb
@@ -7,12 +7,12 @@ describe 'trove::db' do
     context 'with default parameters' do
 
       it { is_expected.to contain_trove_config('database/connection').with_value('sqlite:////var/lib/trove/trove.sqlite').with_secret(true) }
-      it { is_expected.to contain_trove_config('database/idle_timeout').with_value('3600') }
-      it { is_expected.to contain_trove_config('database/min_pool_size').with_value('1') }
-      it { is_expected.to contain_trove_config('database/max_pool_size').with_value('10') }
-      it { is_expected.to contain_trove_config('database/max_overflow').with_value('20') }
-      it { is_expected.to contain_trove_config('database/max_retries').with_value('10') }
-      it { is_expected.to contain_trove_config('database/retry_interval').with_value('10') }
+      it { is_expected.to contain_trove_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_trove_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_trove_config('database/max_pool_size').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_trove_config('database/max_overflow').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_trove_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_trove_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
 
     end
 
@@ -90,10 +90,10 @@ describe 'trove::db' do
       end
 
       it 'install the proper backend package' do
-        is_expected.to contain_package('trove-backend-package').with(
+        is_expected.to contain_package('db_backend_package').with(
           :ensure => 'present',
           :name   => 'python-pymysql',
-          :tag    => ['openstack', 'trove-package'],
+          :tag    => 'openstack',
         )
       end
     end
@@ -114,7 +114,7 @@ describe 'trove::db' do
         { :database_connection     => 'mysql+pymysql://trove:trove@localhost/trove', }
       end
 
-      it { is_expected.not_to contain_package('trove-backend-package') }
+      it { is_expected.not_to contain_package('db_backend_package') }
     end
   end
 
