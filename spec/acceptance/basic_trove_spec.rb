09de2784ee0aa92462499127d7d0cd7e3969a107
ZhongShengping <chdzsp@163.com>
2018-03-15 14:28:56 +0800

    Switch acceptance test to use integration classes
    Change-Id: I92d8f7657c9ff097f7f23244c7452478621199ee

--- spec/acceptance/basic_trove_spec.rb
+++ spec/acceptance/basic_trove_spec.rb
@@ -11,52 +11,7 @@ describe 'basic trove' do
       include ::openstack_integration::rabbitmq
       include ::openstack_integration::mysql
       include ::openstack_integration::keystone
-
-      rabbitmq_user { 'trove':
-        admin    => true,
-        password => 'an_even_bigger_secret',
-        provider => 'rabbitmqctl',
-        require  => Class['rabbitmq'],
-      }
-
-      rabbitmq_user_permissions { 'trove@/':
-        configure_permission => '.*',
-        write_permission     => '.*',
-        read_permission      => '.*',
-        provider             => 'rabbitmqctl',
-        require              => Class['rabbitmq'],
-      }
-
-      # Trove resources
-      class { '::trove':
-        database_connection   => 'mysql+pymysql://trove:a_big_secret@127.0.0.1/trove?charset=utf8',
-        default_transport_url => 'rabbit://trove:an_even_bigger_secret@127.0.0.1:5672/',
-        nova_proxy_admin_pass => 'a_big_secret',
-      }
-      class { '::trove::db::mysql':
-        password => 'a_big_secret',
-      }
-      class { '::trove::keystone::auth':
-        password => 'a_big_secret',
-      }
-      class { '::trove::keystone::authtoken':
-        password => 'a_big_secret',
-      }
-      class { '::trove::api':
-        debug             => true,
-      }
-      class { '::trove::client': }
-      class { '::trove::conductor':
-        debug   => true,
-      }
-      if ($::operatingsystem == 'Ubuntu') and (versioncmp($::operatingsystemmajrelease, '16') >= 0) {
-        warning('trove::taskmanager is disabled now, not working correctly on Xenial.')
-      } else {
-        class { '::trove::taskmanager':
-          debug   => true,
-        }
-      }
-      class { '::trove::quota': }
+      include ::openstack_integration::trove
       EOS
 
 
