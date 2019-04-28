2cbfbf7d5f0b15f0cf80d2248b448c934df5e9b7
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:14 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: Ifb876685e27df60f400105564b62eaf06be30287
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -8,7 +8,7 @@
 #   Url used to connect to database.
 #   (Optional) Defaults to 'sqlite:////var/lib/trove/trove.sqlite'.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   Timeout when db connections should be reaped.
 #   (Optional) Defaults to 3600.
 #
@@ -37,40 +37,53 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class trove::db (
-  $database_connection     = 'sqlite:////var/lib/trove/trove.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/trove/trove.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::trove::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   # NOTE(spredzy): In order to keep backward compatibility we rely on the pick function
   # to use trove::<myparam> if trove::db::<myparam> isn't specified.
-  $database_connection_real     = pick($::trove::database_connection, $database_connection)
-  $database_idle_timeout_real   = pick($::trove::database_idle_timeout, $database_idle_timeout)
-  $database_min_pool_size_real  = pick($::trove::database_min_pool_size, $database_min_pool_size)
-  $database_max_pool_size_real  = pick($::trove::database_max_pool_size, $database_max_pool_size)
-  $database_max_retries_real    = pick($::trove::database_max_retries, $database_max_retries)
-  $database_retry_interval_real = pick($::trove::database_retry_interval, $database_retry_interval)
-  $database_max_overflow_real   = pick($::trove::database_max_overflow, $database_max_overflow)
+  $database_connection_real              = pick($::trove::database_connection, $database_connection)
+  $database_connection_recycle_time_real = pick($::trove::database_idle_timeout, $database_idle_timeout, $database_connection_recycle_time)
+  $database_min_pool_size_real           = pick($::trove::database_min_pool_size, $database_min_pool_size)
+  $database_max_pool_size_real           = pick($::trove::database_max_pool_size, $database_max_pool_size)
+  $database_max_retries_real             = pick($::trove::database_max_retries, $database_max_retries)
+  $database_retry_interval_real          = pick($::trove::database_retry_interval, $database_retry_interval)
+  $database_max_overflow_real            = pick($::trove::database_max_overflow, $database_max_overflow)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection_real,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'trove_config':
-    connection     => $database_connection_real,
-    idle_timeout   => $database_idle_timeout_real,
-    min_pool_size  => $database_min_pool_size_real,
-    max_pool_size  => $database_max_pool_size_real,
-    max_retries    => $database_max_retries_real,
-    retry_interval => $database_retry_interval_real,
-    max_overflow   => $database_max_overflow_real,
-    pool_timeout   => $database_pool_timeout,
+    connection              => $database_connection_real,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size_real,
+    max_pool_size           => $database_max_pool_size_real,
+    max_retries             => $database_max_retries_real,
+    retry_interval          => $database_retry_interval_real,
+    max_overflow            => $database_max_overflow_real,
+    pool_timeout            => $database_pool_timeout,
   }
 }
--- spec/classes/trove_db_spec.rb
+++ spec/classes/trove_db_spec.rb
@@ -6,42 +6,42 @@ describe 'trove::db' do
       it { should contain_class('trove::deps') }
 
       it { should contain_oslo__db('trove_config').with(
-        :connection     => 'sqlite:////var/lib/trove/trove.sqlite',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :connection              => 'sqlite:////var/lib/trove/trove.sqlite',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_connection     => 'mysql+pymysql://trove:trove@localhost/trove',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_max_pool_size  => '21',
-          :database_max_retries    => '11',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
-          :database_retry_interval => '11',
+          :database_connection              => 'mysql+pymysql://trove:trove@localhost/trove',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_pool_size           => '21',
+          :database_max_retries             => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
+          :database_retry_interval          => '11',
         }
       end
 
       it { should contain_class('trove::deps') }
 
       it { should contain_oslo__db('trove_config').with(
-        :connection     => 'mysql+pymysql://trove:trove@localhost/trove',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '21',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :connection              => 'mysql+pymysql://trove:trove@localhost/trove',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '21',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
