9513b2768a95073ae939a0baea3e9c8e9a039b47
ZhongShengping <chdzsp@163.com>
2020-09-16 11:05:05 +0800

    Set openstackdocs_auto_name to use 'project' as name
    Change-Id: I686ea128d8c50b74855482a68edb8500071fb6aa

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -253,3 +253,4 @@ locale_dirs = ['locale/']
 openstackdocs_repo_name = 'openstack/puppet-trove'
 openstackdocs_bug_project = 'puppet-trove'
 openstackdocs_bug_tag = ''
+openstackdocs_auto_name = False
