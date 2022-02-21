93551f863cf13777dab94c2123fb04fa22a45758
Takashi Kajinami <tkajinam@redhat.com>
2022-02-22 01:05:15 +0900

    Use openstack CLI instad of trove CLI
    This change replaces trove CLI by openstack CLI, so that the providers
    can leverage clouds.yaml generated by puppet-keystone.
    
    Change-Id: Ib848ce76f842255242ef099c0fc04b06ec5e1706

--- spec/unit/provider/trove_datastore/trove_spec.rb
+++ spec/unit/provider/trove_datastore/trove_spec.rb
@@ -6,6 +6,13 @@ provider_class = Puppet::Type.type(:trove_datastore).provider(:trove)
 
 describe provider_class do
 
+  let(:set_env) do
+    ENV['OS_USERNAME']     = 'test'
+    ENV['OS_PASSWORD']     = 'abc123'
+    ENV['OS_PROJECT_NAME'] = 'test'
+    ENV['OS_AUTH_URL']     = 'http://127.0.0.1:5000'
+  end
+
   let :datastore_name do
     'foo'
   end
@@ -23,32 +30,35 @@ describe provider_class do
   end
 
   before :each do
-    described_class.stubs(:list_trove_resources).with('datastore').returns([
-       resource
-    ])
+    set_env
   end
 
   describe "self.instances" do
-    it "should have an instances method" do
-      expect(provider.class).to respond_to(:instances)
-    end
-
-    it "should list instances" do
-      datastores = described_class.instances
-      expect(datastores.size).to eq(1)
-      datastores.map {|provider| provider.name} == datastore_name
+    it 'lists datastores' do
+      provider_class.expects(:openstack)
+        .with('datastore', 'list', '--quiet', '--format', 'csv', [])
+        .returns('"ID","Name"
+"1275b24c-73af-4c51-98ec-c9938a94a153","store1"
+"18088802-efe2-42f8-ac85-ecfddd37d24e","store2"
+')
+      instances = provider_class.instances
+      expect(instances.length).to eq(2)
+      expect(instances[0].id).to eq('1275b24c-73af-4c51-98ec-c9938a94a153')
+      expect(instances[0].name).to eq('store1')
+      expect(instances[1].id).to eq('18088802-efe2-42f8-ac85-ecfddd37d24e')
+      expect(instances[1].name).to eq('store2')
     end
   end
 
   describe '#create' do
-    it 'should call trove-manage' do
-      provider.expects(:trove_manage).with(
-        ['trove-manage', 'datastore_update', datastore_name, "''"]
-      ).returns(0)
+    it 'creates datastore' do
+      provider.expects(:trove_manage)
+        .with(['trove-manage', 'datastore_update', datastore_name, "''"])
+        .returns(0)
 
-      provider.expects(:trove_manage).with(
-        ['trove-manage', 'datastore_update', datastore_name, "0.1"]
-      ).returns(0)
+      provider.expects(:trove_manage)
+        .with(['trove-manage', 'datastore_update', datastore_name, "0.1"])
+        .returns(0)
 
       provider.create
     end
--- spec/unit/provider/trove_datastore_version/trove_spec.rb
+++ spec/unit/provider/trove_datastore_version/trove_spec.rb
@@ -6,12 +6,19 @@ provider_class = Puppet::Type.type(:trove_datastore_version).provider(:trove)
 
 describe provider_class do
 
+  let(:set_env) do
+    ENV['OS_USERNAME']     = 'test'
+    ENV['OS_PASSWORD']     = 'abc123'
+    ENV['OS_PROJECT_NAME'] = 'test'
+    ENV['OS_AUTH_URL']     = 'http://127.0.0.1:5000'
+  end
+
   let :datastore_name do
-    'foo'
+    'mysql'
   end
 
   let :datastore_version do
-    '1.0'
+    '5.7.29'
   end
 
   let :resource do
@@ -30,7 +37,11 @@ describe provider_class do
     described_class.new(resource)
   end
 
-  describe "self.instances" do
+  before :each do
+    set_env
+  end
+
+  describe "#instances" do
     it "should have an instances method" do
       expect(provider.class).to respond_to(:instances)
     end
@@ -39,12 +50,24 @@ describe provider_class do
   describe '#create' do
     it 'should call trove-manage' do
       provider.expects(:trove_manage).with(
-        ['trove-manage', 'datastore_version_update', datastore_name, "1.0",
-         'mysql', '1234', 'mysql', '1']
+        ['trove-manage', 'datastore_version_update', datastore_name,
+         datastore_version, 'mysql', '1234', 'mysql', '1']
       ).returns(0)
 
       provider.create
     end
   end
 
+  describe '#exists' do
+    it 'should list datastore versions' do
+      provider_class.expects(:openstack)
+        .with('datastore version', 'list', '--quiet', '--format', 'csv',
+              datastore_name)
+        .returns('"ID","Name","Version"
+"9c4d3fb1-644c-4543-9c37-49b3a801b66c","5.7.29","5.7.29"
+"406b75fb-0727-4923-a702-d677e3fd84ab","5.7.30","5.7.30"
+')
+      expect(provider.exists?).to be_truthy
+    end
+  end
 end
