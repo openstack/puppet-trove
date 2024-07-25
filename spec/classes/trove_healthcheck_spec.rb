36898524ab0a26f4421f1554d26cfacbf27baeeb
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-07-25 22:48:36 +0900

    Add support for [healthcheck] enable_by_file_paths
    Depends-on: https://review.opendev.org/923648
    Change-Id: I012e0c89bff2e8f03b93a9a4fb7e31e1e192a6b3

--- spec/classes/trove_healthcheck_spec.rb
+++ spec/classes/trove_healthcheck_spec.rb
@@ -17,6 +17,7 @@ describe 'trove::healthcheck' do
           :ignore_proxied_requests => '<SERVICE DEFAULT>',
           :disable_by_file_path    => '<SERVICE DEFAULT>',
           :disable_by_file_paths   => '<SERVICE DEFAULT>',
+          :enable_by_file_paths    => '<SERVICE DEFAULT>',
         )
       end
     end
@@ -30,6 +31,7 @@ describe 'trove::healthcheck' do
           :ignore_proxied_requests => false,
           :disable_by_file_path    => '/etc/trove/healthcheck/disabled',
           :disable_by_file_paths   => ['8779:/etc/trove/healthcheck/disabled'],
+          :enable_by_file_paths    => ['/etc/trove/healthcheck/enabled'],
         }
       end
 
@@ -41,6 +43,7 @@ describe 'trove::healthcheck' do
           :ignore_proxied_requests => false,
           :disable_by_file_path    => '/etc/trove/healthcheck/disabled',
           :disable_by_file_paths   => ['8779:/etc/trove/healthcheck/disabled'],
+          :enable_by_file_paths    => ['/etc/trove/healthcheck/enabled'],
         )
       end
     end
